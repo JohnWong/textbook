804c3cdd308a2575fb1726470a6f6fe01a789c3c
John Wong <huangxiaozhe1988@gmail.com>
2015-04-15 03:01:25 +0800

    翻页

--- textbook/PageViewController.swift
+++ textbook/PageViewController.swift
@@ -12,37 +12,30 @@ class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
     
     override func viewDidLoad() {
         
+        self.dataSource = self
+        
         var viewControllers = Array<UIViewController>()
         for i in 1...1 {
             let viewController: AnyObject! = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("Page")
             if let viewController = viewController as? UIViewController {
+                viewController.view.backgroundColor = UIColor.greenColor()
                 viewControllers.append(viewController)
             }
         }
         
-        self.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: { (Bool) -> Void in
-            //
+        self.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: true, completion: { (result: Bool) -> Void in
+            print("\(result)")
         })
-        
-        
-//        func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-//            return nil
-//        }
-//        
-//        func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-//            return nil
-//        }
-        
-        func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
-        {
-            return UIViewController()
-        }
-        
-        func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController?
-        {
-            return UIViewController()
-        }
+    }
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.redColor()
+        return vc
     }
     
-   
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.blueColor()
+        return vc
+    }
 }
