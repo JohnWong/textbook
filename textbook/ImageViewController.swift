84ba0f1cedd4ea6d418906cf8f34227de3a99a01
John Wong <huangxiaozhe1988@gmail.com>
2015-04-21 10:46:03 +0800

    大图完成

--- textbook/ImageViewController.swift
+++ textbook/ImageViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ImageViewController: UIViewController {
+class ImageViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
     
     var index = 0
     
@@ -25,6 +26,24 @@ class ImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setImagePathIfExists(imagePath)
+        initGestures()
+        initScrollView()
+    }
+    
+    func initScrollView() {
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 2
+        scrollView.zoomScale = 1
+        scrollView.delegate = self
+    }
+    
+    func initGestures() {
+        var singleTap = UITapGestureRecognizer(target: self, action: Selector("didSingleTap"))
+        var doubleTap = UITapGestureRecognizer(target: self, action: Selector("didDoubleTap"))
+        doubleTap.numberOfTapsRequired = 2
+        singleTap.requireGestureRecognizerToFail(doubleTap)
+        self.view.addGestureRecognizer(singleTap)
+        self.view.addGestureRecognizer(doubleTap)
     }
     
     func setImagePathIfExists(url: String) {
@@ -37,15 +56,25 @@ class ImageViewController: UIViewController {
         }
     }
     
-    override func viewWillLayoutSubviews() {
-        self.imageWidthConstraint.constant = self.view.frame.width
-        super.viewWillLayoutSubviews()
+    func didSingleTap() {
+        scrollView.zoomScale = 1
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let controller: AnyObject! = segue.destinationViewController;
-        if let controller = controller as? DetailViewController {
-            controller.imagePath = imagePath
+    func didDoubleTap() {
+        if scrollView.zoomScale != scrollView.maximumZoomScale {
+            scrollView.zoomScale = scrollView.maximumZoomScale
+        } else {
+            scrollView.zoomScale = 1
         }
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.imageWidthConstraint.constant = self.view.frame.width
+        self.imageHeightConstraint.constant = self.view.frame.height
+        super.viewWillLayoutSubviews()
     }
 }
