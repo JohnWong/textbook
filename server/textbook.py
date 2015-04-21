b64c2fe2a78549e6ad67de005228fbf6161d9442
John Wong <huangxiaozhe1988@gmail.com>
2015-04-21 18:03:34 +0800

    继续服务端

--- server/textbook.py
+++ server/textbook.py
@@ -27,11 +27,12 @@ def fetchimg(indexurl, pageurl):
 def requesturl(url):
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
-    result = response.read().decode("gbk")
+    result = response.read()#.decode("gbk")
     return BeautifulSoup(result)
 
 
 def fetchbook(indexurl, filename):
+    print("Book: " + indexurl)
     doc = requesturl(indexurl).recs.documents
     name = None
     pages = []
@@ -66,7 +67,7 @@ def fetchbook(indexurl, filename):
             "link": link,
             "page": page
         })
-        print("%s, %s" % (title, page))
+        # print("%s, %s" % (title, page))
         pass
 
     jsonstr = json.dumps({
@@ -77,5 +78,83 @@ def fetchbook(indexurl, filename):
         f.write(jsonstr)
     pass
 
-indexurl = "http://www.pep.com.cn/xiaoyu/jiaoshi/tbjx/kbjiaocai/tb1s/index_2152.xml"
-fetchbook(indexurl, "index1s.json")
+
+def fetchsubject(url):
+    print("Subject: " + url)
+    doc = requesturl(url)
+    headers = []
+    listurls = []
+    sections = []
+
+    for keyword in ["电子课本", "简谱课本", "线谱课本"]:
+        kb = doc.find("a", text=keyword)
+        if kb:
+            headers.append("电子课本")
+            listurls.append(url + kb.get("href")[2:])
+        pass
+
+    for i in range(len(listurls)):
+        doc = requesturl(listurls[i])
+        rows = []
+        for node in doc.find("table").find_all("td"):
+            span = node.find("span")
+            if not span:
+                continue
+            title = span.find("a").get_text()
+            href = span.find("a").get("href")[2:]
+            link = url + href + "index_2152.xml"
+            img = url + node.find("img").get("src")[2:]
+            # fetch book josn
+            prefix = "www.pep.com.cn/"
+            filename = re.search(prefix + ".*", url + href).group()[len(prefix):-1].replace("/", "-")
+            print(filename)
+            fetchbook(link, filename)
+
+            rows.append({
+                "title": title,
+                "link": filename,
+                "img": img
+            })
+
+        sections.append({
+            "header": headers[i],
+            "rows": rows
+        })
+        pass
+    return sections
+
+
+def fetchindex(filename):
+    url = "http://www.pep.com.cn/"
+    doc = requesturl(url)
+    cates = []
+    xkdh = doc.find(id="xkdh")
+    for node in xkdh.find_all("td"):
+        catdiv = node.find(class_="darkgreen")
+        if not catdiv:
+            continue
+        catname = node.find(class_="darkgreen").get_text().replace(" ", "")
+        subjects = []
+        for subject in node.find_all("a"):
+            link = url + subject.get("href")[2:]
+            subjects.append(fetchsubject(link))
+            pass
+        cates.append({
+            "header": catname,
+            "rows": subjects
+        })
+        pass
+
+    jsonstr = json.dumps({
+        "cates": cates
+    })
+    with open(filename, "w") as f:
+        f.write(jsonstr)
+    print("Index Done!")
+    pass
+
+
+if __name__ == '__main__':
+    # indexurl = "http://www.pep.com.cn/xiaoyu/jiaoshi/tbjx/kbjiaocai/tb1s/index_2152.xml"
+    # fetchbook(indexurl, "index1s.json")
+    fetchindex("index.json")
